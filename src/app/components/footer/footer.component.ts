65fe81d1b4796871064fe9c560ff62087e63f6cf
HaJunHyeok <enoxaiming@naver.com>
2017-03-13 22:51:43 +0900

    Fix styleUrls:footer.component.ts

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'footer-component',
   template: './footer.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./footer.component.css']
 })
 
 export class FooterComponent implements OnInit {
