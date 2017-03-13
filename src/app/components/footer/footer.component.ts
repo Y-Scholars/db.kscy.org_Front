de5529ed064995625ac9ef510411579cb39d994b
HaJunHyeok <enoxaiming@naver.com>
2017-03-13 22:35:52 +0900

    Add Footer & Header Components to manage.

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'footer-component',
+  template: './footer.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class FooterComponent implements OnInit {
+
+    ngOnInit() {
+        
+    }
+
+}
