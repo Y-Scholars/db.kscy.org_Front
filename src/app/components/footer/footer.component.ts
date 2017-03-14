ab8398655e1c3daff115bb87912fb6a25fcd4fe7
dudgns0507 <hoon2585@gmail.com>
2017-03-14 19:46:30 +0900

    Update footer information & style
    - Change body margin to 0px

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'footer-component',
-  template: './footer.component.html',
+  templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 
 export class FooterComponent implements OnInit {
 
     ngOnInit() {
-        
+
     }
 
 }
