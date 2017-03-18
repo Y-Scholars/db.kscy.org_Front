fcd1306bef8a1ddcdc7feee8b2bfadd94fb7c2d3
HaJunHyeok <enoxaiming@naver.com>
2017-03-19 00:59:59 +0900

    Add result.component

---
+++ src/app/components/search/result/result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'result-component',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.css']
+})
+
+export class ResultComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+  
+}
