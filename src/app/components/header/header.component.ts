032a77fab260a312a28fd8e458796abdd3512125
dudgns0507 <hoon2585@gmail.com>
2017-03-22 10:52:17 +0900

    Udpate header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 
+class Login {
+    on:boolean = false;
+
+    toggle() {
+        this.on = !this.on;
+    }
+}
+
+@Component({
+    selector: 'login',
+    template: '<popup><h2>Content</h2>Some content here</popup>', 
+    directives: [Login]
+})
+
 export class HeaderComponent implements OnInit {
 
   ngOnInit() {
 
   }
-  
+
 }
