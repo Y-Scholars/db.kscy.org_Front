6fc2819fdbc1797b05ad3f1538fca1370b68508e
HaJunHyeok <enoxaiming@naver.com>
2017-03-26 12:12:38 +0900

    Add user directory for signup / profile

---
+++ src/app/components/user/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile-component',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+
+export class ProfileComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+  
+}
---
+++ src/app/components/user/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'signup-component',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+
+export class SignUpComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+  
+}
