f491b5bda51939e52ee20c38bb808885bbd27491
HaJunHyeok <enoxaiming@naver.com>
2017-03-28 15:51:41 +0900

    Apply on login component

--- src/app/components/header/login.component.ts
+++ src/app/components/header/login.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 
 import { DialogRef, ModalComponent } from 'angular2-modal';
 import { BSModalContext } from 'angular2-modal/plugins/bootstrap';
+import { LoginService } from '../service/loginService';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'modal-content',
     styleUrls: ['./login.component.css'],
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    providers:[LoginService]
 })
 
 export class LoginWindow {
-    constructor(public dialog: DialogRef<BSModalContext>) {
+    private email : String;
+    private password : String;
+
+    failed = false;
+    constructor(public dialog: DialogRef<BSModalContext>,router:Router,private _loginService:LoginService) {
         // this.dialog.context.dialogClass = 'modal-centered';
+    }
+    onLogin() {
+        this.email = (<HTMLInputElement>document.getElementById("email")).value;
+        this.password = (<HTMLInputElement>document.getElementById("password")).value;
+        this._loginService.login(this.email,this.password);
     }
 }
