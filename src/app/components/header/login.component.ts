899847e4b6832350afc243e40c9a986225c78456
dudgns0507 <hoon2585@gmail.com>
2017-04-02 16:50:38 +0900

    Add Signup
    - Add onSignup() method to signup
    - Add tabClicked() method to change tab
    - Update signup.service

--- src/app/components/header/login.component.ts
+++ src/app/components/header/login.component.ts
@@ -3,32 +3,47 @@ import { Component } from '@angular/core';
 import { DialogRef, ModalComponent } from 'angular2-modal';
 import { BSModalContext } from 'angular2-modal/plugins/bootstrap';
 import {AuthService} from '../service/auth.service';
+import {SignupService} from '../service/signup.service';
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'modal-content',
     styleUrls: ['./login.component.css'],
     templateUrl: './login.component.html',
-    providers: [AuthService]
+    providers: [AuthService, SignupService]
 })
 
 export class LoginWindow {
     private email : String;
     private password : String;
+    private username : String;
+    private grade : String;
+    private birthday : String;
+    private location : String;
+
     private autoLogin : boolean = false;
+    private active : boolean = true;
     //filter: boolean= false;
 
-    constructor(public dialog: DialogRef<BSModalContext>,router:Router,private auth:AuthService) {
+    constructor(public dialog: DialogRef<BSModalContext>,router:Router,private auth:AuthService, private signup:SignupService) {
         // this.dialog.context.dialogClass = 'modal-centered';
     }
 /*
     filterData(){
-        this.filter = !this.filter;// this will change value of it true and false 
+        this.filter = !this.filter;// this will change value of it true and false
         console.log(this.filter);
     }
     */
 
+    tabClicked() {
+        this.active = !this.active;
+    }
+
     onLogin() {
-        this.auth.login(this.email,this.password,this.autoLogin);
+        this.auth.login(this.email, this.password, this.autoLogin);
+    }
+
+    onSignup() {
+        this.signup.signUp(this.username, this.email, this.password, this.grade, this.birthday, this.location);
     }
 }
--- src/app/components/service/signup.service.ts
+++ src/app/components/service/signup.service.ts
@@ -7,13 +7,10 @@ export class SignupService {
     constructor(private http:Http) {
     }
 
-    signUp() {
-        let email = "hoonasda2585@gmail.com";
-        let username = "dudgns0507";
-        let password = "12345";
+    signUp(username:String, email:String, password:String, grade:String, birthday:String, location:String) {
         let url = "http://ec2-54-190-7-146.us-west-2.compute.amazonaws.com:5000/api/v1/users";
 
-        let body = "email=" + email + "&username=" + username + "&password=" + password;
+        let body = "email=" + email + "&username=" + username + "&password=" + password + "&grade=" + grade + "&birthday=" + birthday + "&location=" + location;
 
         let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded', 'Accept':'application/json'});
         let options = new RequestOptions({headers:headers});
@@ -23,6 +20,9 @@ export class SignupService {
                 console.log(res);
                 let json_body = JSON.parse(res['_body']);
                 console.log('json body:', json_body);
+                if(json_body.code == 201) {
+                    alert("회원가입되었습니다.");
+                }
             });
     }
 }
