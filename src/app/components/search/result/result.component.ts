c5a61767581f0d9fd669f29fc0b6b2ba22907a8d
HaJunHyeok <enoxaiming@naver.com>
2017-05-08 22:49:48 +0900

    Result Page Binding

--- src/app/components/search/result/result.component.ts
+++ src/app/components/search/result/result.component.ts
@@ -12,6 +12,17 @@ export class ResultComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private searching:SearchOneService) { }
 
+  research_name : String;
+  author : String;
+  school : String;
+  grade : String;
+  year : String;
+  pages : String;
+  email : String;
+  team_num : number;
+  type : String;
+  fileurl : string;
+
   ngOnInit() {
     let key = this.route.snapshot.url[0].path;
     console.log(key);
@@ -20,9 +31,24 @@ export class ResultComponent implements OnInit {
         res => {
         let obj = JSON.parse(res);
 
+        this.research_name = obj.hits.hits[0]._source.research_name;
+        this.author = obj.hits.hits[0]._source.researcher_name;
+        this.school = obj.hits.hits[0]._source.org;
+        this.grade = obj.hits.hits[0]._source.grade;
+        this.year = "2017";
+        this.pages = "30";
+        this.email = obj.hits.hits[0]._source.email;
+        this.team_num = obj.hits.hits[0]._source.team_num;
+        this.type = obj.hits.hits[0]._source.type;
+        this.fileurl = obj.hits.hits[0]._source.file_url;
+
         console.log(obj);
         }
       );
+  }
+
+  downloadFiles() {
+    window.location.href=this.fileurl;
   }
 
 }
