de78785f2ed1d11f588786bf2614744eb33c9d5f
HaJunHyeok <enoxaiming@naver.com>
2017-05-12 22:55:39 +0900

    Add result contents component

---
+++ src/app/components/search/resultcontents/resultcontents.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { SearchOneService } from '../../service/search-one.service';
+
+@Component({
+  selector: 'resultcontents-component',
+  templateUrl: './resultcontents.component.html',
+  styleUrls: ['./resultcontents.component.css']
+})
+
+export class ResultContentsComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private searching:SearchOneService) { }
+
+  research_name : String;
+  author : String;
+  school : String;
+  grade : String;
+  year : String;
+  pages : String;
+  email : String;
+  team_num : number;
+  type : String;
+  fileurl : string;
+  abstract : String;
+
+  ngOnInit() {
+    let key = this.route.snapshot.url[0].path;
+    console.log(key);
+    var res = this.searching.searchOne(key)
+      .subscribe(
+        res => {
+          let obj = JSON.parse(res);
+
+          this.research_name = obj.hits.hits[0]._source.research_name;
+          this.author = obj.hits.hits[0]._source.researcher_name;
+          this.school = obj.hits.hits[0]._source.org;
+          this.grade = obj.hits.hits[0]._source.grade;
+          this.year = "2017";
+          this.pages = "30";
+          this.email = obj.hits.hits[0]._source.email;
+          this.team_num = obj.hits.hits[0]._source.team_num;
+          this.type = obj.hits.hits[0]._source.type;
+          this.abstract = obj.hits.hits[0]._source.abstract_kor;
+          console.log(obj);
+        }
+      );
+  }
+
+  downloadFiles() {
+    window.location.href=this.fileurl;
+  }
+
+}
