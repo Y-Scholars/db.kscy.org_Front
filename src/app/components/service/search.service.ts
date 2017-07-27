dcccf725800cb1bdfc7bd543207c15403e3d41c3
HaJunHyeok <enoxaiming@naver.com>
2017-07-28 03:00:21 +0900

    Search fin

--- src/app/components/service/search.service.ts
+++ src/app/components/service/search.service.ts
@@ -1,28 +1,37 @@
-import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from "@angular/http";
-import {Observable} from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SearchService {
-    constructor(private http:Http) {
+    constructor(private http: Http) {
     }
 
-    search(keyword : string) {
-        alert(keyword);
-        let url = "http://ec2-54-190-7-146.us-west-2.compute.amazonaws.com:8888/archive/_search?size=200&q="+encodeURIComponent(keyword);
+    search(keyword: string) {
+        let url = "http://db.kscy.org:8888/archive/_search";
+
+        let body = {
+            "query": {
+                "multi_match": {
+                    "query": keyword,
+                    "fields": ["research_name", "abstract_kor", "researcher_name", "org"]
+                }
+            }
+        }
 
         let headers = new Headers({
-            'Content-Type': 'application/json' ,
+            'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers':'X-Requested-With'
+            'Access-Control-Allow-Headers': 'X-Requested-With'
         });
+        headers.append('Access-Control-Max-Age', '3600');
         let options = new RequestOptions({
             headers: headers
         });
 
-        return this.http.get(url)
+        return this.http.post(url,body)
             .map(res => res.text());
     }
 }
